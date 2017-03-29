7191ff563b0267fe3b8159a401d64f66e65b7fe7
yun.wang <rollin12@126.com>
2017-03-29 10:18:43 +0800

    modify it

---
+++ app/src/androidTest/java/edittext/security/com/securityedittextdemo/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package edittext.security.com.securityedittextdemo;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("edittext.security.com.securityedittextdemo", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/edittext/security/com/securityedittextdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package edittext.security.com.securityedittextdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * <pre>
+ * author: yun.wang
+ * Time: 2017/03/28
+ * Description: ${Demo Acitivty for frameEditText}
+ * Version: ${1.1}
+ * </pre>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
